0951c3a9558a8bdfd47ed770a0a90601f5dd6038
Manda <imanda.minara@outlook.com>
2017-10-23 13:47:57 +0700

    [front-end] view all products

--- src/main/java/com/uts/apap/controller/ProductController.java
+++ src/main/java/com/uts/apap/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.uts.apap.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.uts.apap.model.ProductModel;
 import com.uts.apap.service.ProductService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +26,9 @@ public class ProductController {
 	
 	@RequestMapping("/product/viewall")
 	public String viewAll(Model model) {
+		List<ProductModel> listProduk = productDAO.getAllProducts();
+		
+		model.addAttribute("listProduk", listProduk);
 		model.addAttribute("title", "Daftar Seluruh Produk");
 		return "view-all";
 	}
