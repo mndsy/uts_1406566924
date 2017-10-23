0cbf572e6eccaf608a482b6b37ebbb8582962178
Manda <imanda.minara@outlook.com>
2017-10-23 12:54:11 +0700

    setting up environment

---
+++ src/main/java/com/uts/apap/UtsApplication.java
@@ -0,0 +1,12 @@
+package com.uts.apap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtsApplication.class, args);
+	}
+}
