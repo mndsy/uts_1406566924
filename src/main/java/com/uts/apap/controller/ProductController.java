379969d8dba87dba900f182833d2bb15458b0a75
Manda <imanda.minara@outlook.com>
2017-10-23 15:31:54 +0700

    [front-end] showing alert if success update product

--- src/main/java/com/uts/apap/controller/ProductController.java
+++ src/main/java/com/uts/apap/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.uts.apap.model.ProductModel;
 import com.uts.apap.service.ProductService;
@@ -35,9 +36,11 @@ public class ProductController {
 	}
 	
 	@RequestMapping("/product/view/{id}")
-	public String view(Model model, @PathVariable(value="id") int id) {
+	public String view(Model model, @PathVariable(value="id") int id, 
+			@RequestParam(value="statusUpdate", required=false) String statusUpdate) {
 		ProductModel product = productDAO.getProduct(id);
 		
+		model.addAttribute("statusUpdate", statusUpdate);
 		model.addAttribute("produk", product);
 		model.addAttribute("title", "Detail Produk " + product.getNama());
 		return "view-detail";
@@ -50,5 +53,33 @@ public class ProductController {
 		model.addAttribute("produk", product);
 		model.addAttribute("title", "Ubah Data Produk " + product.getNama());
 		return "form-update";
+	}
+	
+	@RequestMapping("/product/update/submit")
+	public String updateSubmit(Model model, @RequestParam(value="id") int id,
+			@RequestParam(value="nama") String nama,
+			@RequestParam(value="deskripsi") String deskripsi,
+			@RequestParam(value="kategori") String kategori,
+			@RequestParam(value="pabrikan") String pabrikan,
+			@RequestParam(value="berat") double berat,
+			@RequestParam(value="harga") int harga,
+			@RequestParam(value="tahunProduksi") int tahunProduksi,
+			@RequestParam(value="jumlahStok") int jumlahStok,
+			@RequestParam(value="kondisi") String kondisi) {
+		ProductModel product = productDAO.getProduct(id);
+		product.setNama(nama);
+		product.setDeskripsi(deskripsi);
+		product.setKategori(kategori);
+		product.setPabrikan(pabrikan);
+		product.setBerat(berat);
+		product.setHarga(harga);
+		product.setTahunProduksi(tahunProduksi);
+		product.setJumlahStok(jumlahStok);
+		product.setKondisi(kondisi);
+		
+		productDAO.updateProduct(product);
+		
+		model.addAttribute("title", "Detail Produk " + product.getNama());
+		return "redirect:/product/view/" + id + "?statusUpdate=success";
 	}
 }
